c031644a503d54ddab583b5c23fd5b98925ec7f9
Alejandro Lopez <alejandro.lopezde@gmail.com>
2022-02-25 16:25:15 -0500

    creacion de nav

--- Nav/nav.js
+++ Nav/nav.js
@@ -1,4 +1,14 @@
-ShadowRoot
-kasdkl
-asdkljsad
-lasdjl
+openNavButton = document.getElementById('top-bar');
+nav = document.getElementById('nav')
+
+openedNav = false;
+
+openNavButton.addEventListener("click", ()=>{
+    if (!openedNav) {
+        nav.style.animation= "aparecer 1s forwards"
+        openedNav = true
+  } else {
+        nav.style.animation= "desaparecer 1s forwards"
+        openedNav = false;
+    }
+})
